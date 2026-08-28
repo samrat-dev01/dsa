58191ecc8ab789f4432001156c2856c0ac3e92db
samrat-dev01 <samratb601@gmail.com>
2026-08-28 11:45:02 +0530

    refactor: update folder scanning logic to iterate over predefined directories

--- list-folders.js
+++ list-folders.js
@@ -1,6 +1,16 @@
-import * as fs from "node:fs"
+import * as fs from "node:fs";
 
+// Hardcoded array of directories to scan
+const targetFolders = ["algorithms", "data_structures"];
 
-fs.readdirSync('.', { withFileTypes: true })
-  .filter(item => item.isDirectory())
-  .forEach(dir => console.log(dir.name));
+targetFolders.forEach(targetPath => {
+  console.log(`\n--- Inside: ${targetPath} ---`);
+  
+  try {
+    fs.readdirSync(targetPath, { withFileTypes: true })
+      .filter(item => item.isDirectory())
+      .forEach(dir => console.log(`  ${dir.name}`));
+  } catch (error) {
+    console.error(`  Error reading path "${targetPath}":`, error.message);
+  }
+});
